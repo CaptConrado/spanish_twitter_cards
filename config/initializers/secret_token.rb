64b17369b23c032f18e9b99c101fb8fc4eebd9ab
captconrado <ottey001@gmail.com>
2013-06-21 02:15:39 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spanishswipe::Application.config.secret_token = '0a5e19ea31cbb5d7c49fac96259ecd0bab08fac74301d9565fd342b7f1c53d57351c8c99cb0ea181920e2ca99893a0ce25cf93c90c1b52a969fd7dff54844f85'
